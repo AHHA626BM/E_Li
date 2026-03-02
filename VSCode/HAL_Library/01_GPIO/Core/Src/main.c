9e398a33578d8fcb45acb6eb807040bf1d72a07e
AHHA626BM <16427199862@qq.com>
2026-03-02 22:55:02 +0800

    26.03.02

--- VSCode/HAL_Library/01_GPIO/Core/Src/main.c
+++ VSCode/HAL_Library/01_GPIO/Core/Src/main.c
@@ -19,9 +19,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "gpio.h"
-#include "stm32f103xb.h"
-#include "stm32f1xx_hal.h"
-#include "stm32f1xx_hal_gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -52,60 +49,6 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-void LED_Chaser_1() {
-  for (uint8_t i = 0; i < 8; i++) {
-    HAL_GPIO_WritePin(GPIOB, 0x01<<i << 8 | 0x00, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, ~(0x01 << i) << 8 | 0x00 << i, GPIO_PIN_SET);
-    HAL_Delay(500);
-  }
-  for(uint8_t i = 0; i < 8; i++) {
-    HAL_GPIO_WritePin(GPIOB, 0x80 >> i << 8 | 0x00, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, ~(0x80 >> i) << 8 | 0x00 << i, GPIO_PIN_SET);
-    HAL_Delay(500);
-  }
-}
-
-void LED_Chaser_2() {
-  for (uint8_t i = 0; i < 4; i++) {
-    HAL_GPIO_WritePin(GPIOB, (0x01 << i | 0x80 >> i) << 8 | 0x00, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, ~(0x01 << i | 0x80 >> i) << 8 | 0x00 << i, GPIO_PIN_SET);
-    HAL_Delay(500);
-  }
-  for(uint8_t i = 0; i < 4; i++) {
-    HAL_GPIO_WritePin(GPIOB, (0x08 >> i | 0x10 << i) << 8 | 0x00,
-                      GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, ~(0x08 >> i | 0x10 << i) << 8 | 0x00 << i,
-                      GPIO_PIN_SET);
-    HAL_Delay(500);
-  }
-}
-
-void LED_Chaser_3()
-{
-  HAL_GPIO_WritePin(GPIOB, 0xf0 << 8 | 0x00, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOB, 0x0f << 8 | 0x00, GPIO_PIN_SET);
-  HAL_Delay(500);
-  HAL_GPIO_WritePin(GPIOB, 0xf0 << 8 | 0x00, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(GPIOB, 0x0f << 8 | 0x00, GPIO_PIN_RESET);
-  HAL_Delay(500);
-}
-
-void LED_Chaser_4() {
-  for (uint8_t i = 0; i < 8; i++) {
-    HAL_GPIO_WritePin(GPIOB, 0x01 << i << 8 | 0x00, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, ~(0x01 << i) << 8 | 0x00 << i, GPIO_PIN_SET);
-    HAL_Delay(500);
-    HAL_GPIO_TogglePin(GPIOB, 0xff << 8 | 0x00);
-    HAL_Delay(500);
-  }
-  for (uint8_t i = 0; i < 8; i++) {
-    HAL_GPIO_WritePin(GPIOB, 0x80 >> i << 8 | 0x00, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOB, ~(0x80 >> i) << 8 | 0x00 << i, GPIO_PIN_SET);
-    HAL_Delay(500);
-    HAL_GPIO_TogglePin(GPIOB, 0xff << 8 | 0x00);
-    HAL_Delay(500);
-  }
-}
 
 /* USER CODE END PFP */
 
@@ -122,6 +65,7 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -143,27 +87,15 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  for (uint8_t i = 0; i < 5; i++) {
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOB,
-                    GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 |
-                        GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
-                    GPIO_PIN_RESET);
-  HAL_Delay(500);
-  HAL_GPIO_WritePin(GPIOB,
-                    GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 |
-                        GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
-                    GPIO_PIN_SET);
-  HAL_Delay(500);
-  }
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
-  while (1) {
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1));
-
+  while (1)
+  {
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -183,10 +115,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -196,12 +131,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
